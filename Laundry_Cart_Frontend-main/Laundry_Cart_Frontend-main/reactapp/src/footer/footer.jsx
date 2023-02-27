c92d4f59ffb3449b3e7b9dedcd0a0d0da563f694
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2023-02-27 15:34:07 +0530

    Add files via upload

---
+++ Laundry_Cart_Frontend-main/Laundry_Cart_Frontend-main/reactapp/src/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mainPage-footer">
+        <div>2023 © Laundry</div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
